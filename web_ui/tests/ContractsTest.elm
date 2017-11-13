cc6ec54f0d5c71b14ff49c631e03329a57987ec1
dexterlb <dexterlb@qtrp.org>
2017-11-13 11:04:50 +0200

    test for parsing delegates

--- web_ui/tests/ContractsTest.elm
+++ web_ui/tests/ContractsTest.elm
@@ -4,6 +4,8 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, list, int, string, float)
 import Test exposing (..)
 
+import Dict
+
 import Json.Encode
 
 import Contracts exposing (..)
@@ -25,6 +27,22 @@ suite =
         (parseContract (Json.Encode.encode 4 (Json.Encode.float (f + 0.42))))
         (Ok (FloatValue (f + 0.42)))
     , todo "parse map and list contracts"
-    , todo "parse delegate contracts"
+    , test "can parse a delegate object" <|
+      \_ -> Expect.equal
+        (parseContract """
+          {
+            "__type__": "delegate",
+            "destination": 42,
+            "data": {"foo": "bar"}
+          }
+        """)
+        (Ok (
+          Delegate {
+            destination = 42,
+            data = Dict.fromList [
+              ("foo", "bar")
+            ]
+          }
+        ))
     , todo "parse function contracts"
     ]
