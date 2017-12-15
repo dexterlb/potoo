a88989691fd33f3f77f7a918f4654203fe484ca2
dexterlb <dexterlb@qtrp.org>
2017-12-15 16:24:16 +0200

    implement setting contract in reverse endpoint

--- apps/ui/lib/ui/stream_server/reverse_endpoint.ex
+++ apps/ui/lib/ui/stream_server/reverse_endpoint.ex
@@ -1,9 +1,28 @@
 defmodule Ui.StreamServer.ReverseEndpoint do
   use GenServer
   
-  def handle_call(call, from, parent) do
+  def init(parent) do
+    {:ok, {parent, nil}}
+  end
+
+  def handle_call(call, from = {sender, _}, state = {parent, _}) do
+    case sender == parent do
+      false -> handle_regular_call(call, from, state)
+      true  -> handle_parent_call(call, from, state)
+    end
+  end
+
+  def handle_parent_call({:set_contract, new_contract}, _from, state = {parent, contract}) do
+    {:reply, :ok, {parent, new_contract}}
+  end
+
+  def handle_parent_call(call, from, state) do
+    raise "did you try calling yourself?"
+  end
+
+  def handle_regular_call(call, from, state = {parent, _contract}) do
     send(parent, {:incoming_call, from, call})
-    {:noreply, parent}
+    {:noreply, state}
   end
 
   def start_link(parent) do
