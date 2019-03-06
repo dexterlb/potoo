9fd274d0bbc3c52d87c3a7dd4c41ac87c6c8d868
dexterlb <dexterlb@qtrp.org>
2019-03-06 13:39:44 +0200

    nuke all the elixir

--- potoo_mqtt/test/mqtt_mesh_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PotooMqttTest do
-  use ExUnit.Case
-  doctest PotooMqtt
-
-  test "greets the world" do
-    assert PotooMqtt.hello() == :world
-  end
-end
--- run_testing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-cdir="$(dirname "${0}")"
-
-tmux \
-    new-session  "cd '${cdir}/potoo_global_registry' ; iex --sname reg -S mix ; read x" \; \
-    split-window "cd '${cdir}/potoo_server'          ; iex --sname srv -S mix ; read x" \; \
-    split-window "cd '${cdir}/web_ui'                ; npm run install-deps && npm run serve ; read x" \; \
-    select-layout even-vertical
