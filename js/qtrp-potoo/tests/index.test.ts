8143b35af67488cff07efd907dac827055194b9b
dexterlb <dexterlb@qtrp.org>
2019-11-05 21:22:31 +0200

    add testing framework

---
+++ js/qtrp-potoo/tests/index.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import * as potoo from '../src';
+const fs = eval('require("fs")');
+
+describe('universe', () => {
+  it('works', () => {
+    let result = 6 * 7
+    expect(result).equal(42)
+  })
+});
