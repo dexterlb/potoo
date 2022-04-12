a31695d1ce792d31f46b49d8f44deb6ab7aef1bf
dexterlb <dexterlb@qtrp.org>
2022-04-12 15:32:18 +0300

    implement paho wrapper

--- go/potoo/mqtt/wrappers/pahowrapper/wrapper.go
+++ go/potoo/mqtt/wrappers/pahowrapper/wrapper.go
@@ -1,14 +1,17 @@
 package pahowrapper
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/dexterlb/potoo/go/potoo/mqtt"
 	paho "github.com/eclipse/paho.mqtt.golang"
 )
 
 type Opts struct {
-	Protocol       string
 	BrokerHostname string
 	ClientID       string
+	Qos            int
 	DebugLogger    paho.Logger
 	WarnLogger     paho.Logger
 	ErrorLogger    paho.Logger
@@ -18,7 +21,7 @@ type Opts struct {
 
 type Wrapper struct {
 	opts     *Opts
-	cli      *paho.Client
+	client   paho.Client
 	connConf *mqtt.ConnectConfig
 }
 
@@ -36,31 +39,51 @@ func (p *Wrapper) handleError(err error) {
 	p.connConf.OnDisconnect <- err
 }
 
-func (p *Wrapper) handleMessage(topic []byte, payload []byte) {
-	p.debug(" <- %s : %s", string(topic), string(payload))
-	p.connConf.OnMessage <- mqtt.Message{
-		Topic:   topic,
-		Payload: payload,
-		Retain:  false, // TODO: although not very useful, it would be nice to get this
+func (p *Wrapper) handleToken(token paho.Token) {
+	err := unwrap(token)
+	if err != nil {
+		p.handleError(err)
 	}
+}
+
+func (p *Wrapper) handleMessage(_client paho.Client, msg paho.Message) {
+	topic := msg.Topic()
+	payload := msg.Payload()
+	retained := msg.Retained()
+
+	p.debug(" <- %s : %s", topic, string(payload))
+	msg := mqtt.Message{
+		Topic:   []byte(topic),
+		Payload: payload,
+		Retain:  retained,
+	}
+
+	// we use a goroutine here just to be safe (paho doesn't like its handler to block)
+	// maybe a buffered channel would be better?
+	go func() {
+		p.connConf.OnMessage <- msg
+	}()
 }
 
 func (p *Wrapper) Publish(m mqtt.Message) {
 	p.debug(" -> %s : %s", string(m.Topic), string(m.Payload))
-	panic("not implemented")
-	// if err != nil {
-	// 	p.handleError(err)
-	// }
+	p.handleToken(
+		p.client.Publish(string(m.Topic), byte(p.opts.Qos), m.Retain, m.Payload),
+	)
 }
 
 func (p *Wrapper) Subscribe(filter mqtt.Topic) {
 	p.debug("Subscribe %s", string(filter))
-	panic("not implemented")
+	p.handleToken(
+		p.client.Subscribe(string(filter), byte(p.opts.Qos), nil),
+	)
 }
 
 func (p *Wrapper) Unsubscribe(filter mqtt.Topic) {
 	p.debug("unsubscribe %s", string(filter))
-	panic("not implemented")
+	p.handleToken(
+		p.client.Unsubscribe(string(filter)),
+	)
 }
 
 func (p *Wrapper) Connect(config *mqtt.ConnectConfig) error {
@@ -69,7 +92,17 @@ func (p *Wrapper) Connect(config *mqtt.ConnectConfig) error {
 	paho.WARN = p.opts.WarnLogger
 	paho.ERROR = p.opts.ErrorLogger
 	p.debug("connect with will %s : %s", string(config.WillMessage.Topic), string(config.WillMessage.Payload))
-	panic("not implemented")
+
+	opts := paho.NewClientOptions()
+	opts.AddBroker(p.opts.BrokerHostname)
+	opts.SetClientID(p.opts.ClientID)
+
+	opts.SetKeepAlive(60 * time.Second)
+	opts.SetDefaultPublishHandler(p.handleMessage)
+	opts.SetPingTimeout(1 * time.Second)
+
+	p.client = paho.NewClient(opts)
+	return unwrap(p.client.Connect())
 }
 
 func (p *Wrapper) DisconnectWithWill() {
@@ -85,4 +118,11 @@ func (p *Wrapper) Disconnect() {
 
 func (p *Wrapper) debug(s string, args ...interface{}) {
 	p.opts.DebugLogger.Printf(s, args...)
+}
+
+func unwrap(token paho.Token) error {
+	if !token.Wait() {
+		return fmt.Errorf("mqtt token Wait() returned false")
+	}
+	return token.Error()
 }
